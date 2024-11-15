ec034176e5d761f04c3df4e405ff3fec6d2d413b
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2024-11-16 01:53:10 +0600

    added_app_colors

--- lib/app/utils/app_colors.dart
+++ lib/app/utils/app_colors.dart
@@ -4,6 +4,7 @@ class AppColors {
   // ========= basic colors ==========
   static const Color whiteColor = Color(0xffFFFFFF);
   static const Color blackColor = Color(0xff000000);
+
   // ========= purple colors ========
   static const Color purpleShade50 = Color(0xffFAF5FF);
   static const Color purpleShade100 = Color(0xffF3E8FF);
@@ -15,4 +16,184 @@ class AppColors {
   static const Color purpleShade700 = Color(0xff7E22CE);
   static const Color purpleShade800 = Color(0xff6B21A8);
   static const Color purpleShade900 = Color(0xff581c87);
+
+  // ========= grey colors ==========
+  static const Color greyShade50 = Color(0xffF9FAFB);
+  static const Color greyShade100 = Color(0xffF3F4F6);
+  static const Color greyShade200 = Color(0xffE5E7EB);
+  static const Color greyShade300 = Color(0xffD1D5DB);
+  static const Color greyShade400 = Color(0xff9CA3AF);
+  static const Color greyShade500 = Color(0xff6B7280);
+  static const Color greyShade600 = Color(0xff4B5563);
+  static const Color greyShade700 = Color(0xff374151);
+  static const Color greyShade800 = Color(0xff1F2937);
+  static const Color greyShade900 = Color(0xff111827);
+
+  // ========= green colors ==========
+  static const Color greenShade50 = Color(0xffF0FDF4);
+  static const Color greenShade100 = Color(0xffDCFCE7);
+  static const Color greenShade200 = Color(0xffBBF7D0);
+  static const Color greenShade300 = Color(0xff86EFAC);
+  static const Color greenShade400 = Color(0xff4ADE80);
+  static const Color greenShade500 = Color(0xff22C55E);
+  static const Color greenShade600 = Color(0xff16A34A);
+  static const Color greenShade700 = Color(0xff15803D);
+  static const Color greenShade800 = Color(0xff166534);
+  static const Color greenShade900 = Color(0xff14532D);
+
+  // ========= orange colors ==========
+  static const Color orangeShade50 = Color(0xffFFFBEB);
+  static const Color orangeShade100 = Color(0xffFEF3C7);
+  static const Color orangeShade200 = Color(0xffFDE68A);
+  static const Color orangeShade300 = Color(0xffFCD34D);
+  static const Color orangeShade400 = Color(0xffFBBF24);
+  static const Color orangeShade500 = Color(0xffF59E0B);
+  static const Color orangeShade600 = Color(0xffD97706);
+  static const Color orangeShade700 = Color(0xffB45309);
+  static const Color orangeShade800 = Color(0xff92400E);
+  static const Color orangeShade900 = Color(0xff78350F);
+
+  // ========= red colors ==========
+  static const Color redShade50 = Color(0xffFEF2F2);
+  static const Color redShade100 = Color(0xffFEE2E2);
+  static const Color redShade200 = Color(0xffFECACA);
+  static const Color redShade300 = Color(0xffFCA5A5);
+  static const Color redShade400 = Color(0xffF87171);
+  static const Color redShade500 = Color(0xffEF4444);
+  static const Color redShade600 = Color(0xffDC2626);
+  static const Color redShade700 = Color(0xffB91C1C);
+  static const Color redShade800 = Color(0xff991B1B);
+  static const Color redShade900 = Color(0xff7F1D1D);
+
+  // ========= slate colors ==========
+  static const Color slateShade50 = Color(0xffF8FAFC);
+  static const Color slateShade100 = Color(0xffF1F5F9);
+  static const Color slateShade200 = Color(0xffE2E8F0);
+  static const Color slateShade300 = Color(0xffCBD5E1);
+  static const Color slateShade400 = Color(0xff94A3B8);
+  static const Color slateShade500 = Color(0xff64748B);
+  static const Color slateShade600 = Color(0xff475569);
+  static const Color slateShade700 = Color(0xff334155);
+  static const Color slateShade800 = Color(0xff1E293B);
+  static const Color slateShade900 = Color(0xff0F172A);
+
+  // ========= ash colors ==========
+  static const Color ashShade50 = Color(0xffF9FAFB);
+  static const Color ashShade100 = Color(0xffF3F4F6);
+  static const Color ashShade200 = Color(0xffE5E7EB);
+  static const Color ashShade300 = Color(0xffD1D5DB);
+  static const Color ashShade400 = Color(0xff9CA3AF);
+  static const Color ashShade500 = Color(0xff6B7280);
+  static const Color ashShade600 = Color(0xff4B5563);
+  static const Color ashShade700 = Color(0xff374151);
+  static const Color ashShade800 = Color(0xff1F2937);
+  static const Color ashShade900 = Color(0xff111827);
+
+  // ========= zinc colors ==========
+  static const Color zincShade50 = Color(0xffFAFAFA);
+  static const Color zincShade100 = Color(0xffF4F4F5);
+  static const Color zincShade200 = Color(0xffE4E4E7);
+  static const Color zincShade300 = Color(0xffD4D4D8);
+  static const Color zincShade400 = Color(0xffA1A1AA);
+  static const Color zincShade500 = Color(0xff71717A);
+  static const Color zincShade600 = Color(0xff52525B);
+  static const Color zincShade700 = Color(0xff3F3F46);
+  static const Color zincShade800 = Color(0xff27272A);
+  static const Color zincShade900 = Color(0xff18181B);
+
+  // ========= stone colors ==========
+  static const Color stoneShade50 = Color(0xffFAFAF9);
+  static const Color stoneShade100 = Color(0xffF5F5F4);
+  static const Color stoneShade200 = Color(0xffE7E5E4);
+  static const Color stoneShade300 = Color(0xffD6D3D1);
+  static const Color stoneShade400 = Color(0xffA8A29E);
+  static const Color stoneShade500 = Color(0xff78716C);
+  static const Color stoneShade600 = Color(0xff57534E);
+  static const Color stoneShade700 = Color(0xff44403C);
+  static const Color stoneShade800 = Color(0xff292524);
+  static const Color stoneShade900 = Color(0xff1C1917);
+
+  // ========= teal colors ==========
+  static const Color tealShade50 = Color(0xffF0FDFA);
+  static const Color tealShade100 = Color(0xffCCFBF1);
+  static const Color tealShade200 = Color(0xff99F6E4);
+  static const Color tealShade300 = Color(0xff5EEAD4);
+  static const Color tealShade400 = Color(0xff2DD4BF);
+  static const Color tealShade500 = Color(0xff14B8A6);
+  static const Color tealShade600 = Color(0xff0D9488);
+  static const Color tealShade700 = Color(0xff0F766E);
+  static const Color tealShade800 = Color(0xff115E59);
+  static const Color tealShade900 = Color(0xff134E4A);
+
+  // ========= emerald colors ==========
+  static const Color emeraldShade50 = Color(0xffECFDF5);
+  static const Color emeraldShade100 = Color(0xffD1FAE5);
+  static const Color emeraldShade200 = Color(0xffA7F3D0);
+  static const Color emeraldShade300 = Color(0xff6EE7B7);
+  static const Color emeraldShade400 = Color(0xff34D399);
+  static const Color emeraldShade500 = Color(0xff10B981);
+  static const Color emeraldShade600 = Color(0xff059669);
+  static const Color emeraldShade700 = Color(0xff047857);
+  static const Color emeraldShade800 = Color(0xff065F46);
+  static const Color emeraldShade900 = Color(0xff064E3B);
+
+  // ========= lime colors ==========
+  static const Color limeShade50 = Color(0xffF7FEE7);
+  static const Color limeShade100 = Color(0xffECFCCB);
+  static const Color limeShade200 = Color(0xffD9F99D);
+  static const Color limeShade300 = Color(0xffBEF264);
+  static const Color limeShade400 = Color(0xffA3E635);
+  static const Color limeShade500 = Color(0xff84CC16);
+  static const Color limeShade600 = Color(0xff65A30D);
+  static const Color limeShade700 = Color(0xff4D7C0F);
+  static const Color limeShade800 = Color(0xff3F6212);
+  static const Color limeShade900 = Color(0xff365314);
+
+  // ========= sky colors ==========
+  static const Color skyShade50 = Color(0xffF0F9FF);
+  static const Color skyShade100 = Color(0xffE0F2FE);
+  static const Color skyShade200 = Color(0xffBAE6FD);
+  static const Color skyShade300 = Color(0xff7DD3FC);
+  static const Color skyShade400 = Color(0xff38BDF8);
+  static const Color skyShade500 = Color(0xff0EA5E9);
+  static const Color skyShade600 = Color(0xff0284C7);
+  static const Color skyShade700 = Color(0xff0369A1);
+  static const Color skyShade800 = Color(0xff075985);
+  static const Color skyShade900 = Color(0xff0C4A6E);
+
+  // ========= deepPurple colors ==========
+  static const Color deepPurpleShade50 = Color(0xffFDF4FF);
+  static const Color deepPurpleShade100 = Color(0xffFAE8FF);
+  static const Color deepPurpleShade200 = Color(0xffF5D0FE);
+  static const Color deepPurpleShade300 = Color(0xffF0ABFC);
+  static const Color deepPurpleShade400 = Color(0xffE879F9);
+  static const Color deepPurpleShade500 = Color(0xffD946EF);
+  static const Color deepPurpleShade600 = Color(0xffC026D3);
+  static const Color deepPurpleShade700 = Color(0xffA21CAF);
+  static const Color deepPurpleShade800 = Color(0xff86198F);
+  static const Color deepPurpleShade900 = Color(0xff701A75);
+
+  // ========= pink colors ==========
+  static const Color pinkShade50 = Color(0xffFDF2F8);
+  static const Color pinkShade100 = Color(0xffFCE7F3);
+  static const Color pinkShade200 = Color(0xffFBCFE8);
+  static const Color pinkShade300 = Color(0xffF9A8D4);
+  static const Color pinkShade400 = Color(0xffF472B6);
+  static const Color pinkShade500 = Color(0xffEC4899);
+  static const Color pinkShade600 = Color(0xffDB2777);
+  static const Color pinkShade700 = Color(0xffBE185D);
+  static const Color pinkShade800 = Color(0xff9D174D);
+  static const Color pinkShade900 = Color(0xff831843);
+
+  // ========= rose colors ==========
+  static const Color roseShade50 = Color(0xffFFF1F2);
+  static const Color roseShade100 = Color(0xffFFE4E6);
+  static const Color roseShade200 = Color(0xffFECDD3);
+  static const Color roseShade300 = Color(0xffFDA4AF);
+  static const Color roseShade400 = Color(0xffFB7185);
+  static const Color roseShade500 = Color(0xffF43F5E);
+  static const Color roseShade600 = Color(0xffE11D48);
+  static const Color roseShade700 = Color(0xffBE123C);
+  static const Color roseShade800 = Color(0xff9F1239);
+  static const Color roseShade900 = Color(0xff881337);
 }
